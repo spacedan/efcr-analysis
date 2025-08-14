5d9943084762844f365ccaf293b5c3607325f89e
Danny Huston <danny.huston@ampsight.com>
2025-08-14 08:09:18 -0400

    Reconfigure for remote s3 state and OIDC connections

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,16 @@
 terraform {
   required_version = ">= 1.5.0"
+  
+  backend "s3" {
+    bucket  = "tf-state-bucket-danny-ecfr"
+    key     = "ecfr-analysis/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+    
+    # Optional: Enable state locking with DynamoDB
+    dynamodb_table = "terraform-state-lock"
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
